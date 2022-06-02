de7936ea2275bcfddd84120db546c65b9fc0611e
Fatih Altinok <photonia@gmail.com>
2022-06-02 16:52:02 +0300

    Remove useless test

--- src/routes/Today.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from "@testing-library/react";
-import Today from "./Today";
-
-test("renders correctly", () => {
-  const { getByText } = render(<Today />);
-  const element = getByText(/what did you eat/i);
-  expect(element).toBeInTheDocument();
-});
